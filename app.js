8fe3d1d5c1bb26568c9f05e34c04b6965ffdc207
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-12-14 22:38:19 +0100

    x

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use(
 	cors({
 		credentials: true,
 		// origin: [ 'http://localhost:3000' ] // <== this will be the URL of our React app (it will be running on port 3000)
-		origin: [ 'https://project-management-front.netlify.app' ] // <== this will be the URL of our React app (it will be running on port 3000)
+		origin: [ 'https://project-management-front.herokuapp.com' ] // <== this will be the URL of our React app (it will be running on port 3000)
 	})
 );
 
